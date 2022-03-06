1cda87ecfbe7a22c550cd5c7abcabe342db3b138
Giedrius Macevičius <gmacev@gmail.com>
2022-03-06 14:50:04 +0200

    minor bug fixes, readme update

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 🌻 seeds
 
-Add your targets to targets.txt
+**Installation:**
+
+1. package.json:
+
+    `"homepage": "https://yourwebsite.com/dir"`
+
+2. npm run build
+3. Optional: add your targets to targets.txt
--- src/App.js
+++ src/App.js
@@ -16,9 +16,7 @@ function App() {
     {
         let targets = [], targetsData = []
 
-        console.log()
-
-        fetch(window.location.pathname+'/targets.txt').then((response) => response.text())
+        fetch(window.location.pathname+'targets.txt').then((response) => response.text())
             .then((data) => {
                 targets = data.split('\r\n')
                 setTargets(targets)
@@ -88,9 +86,7 @@ function App() {
                 arr[index].id = index
                 arr[index].online = online
                 online && arr[index].requests++
-
-                if(error)
-                    arr[index].errors++
+                error && arr[index].errors++
 
                 arr.sort((a, b) => (!!a.online === b.online) ? 0 : !!a.online ? -1 : 1)
 
@@ -101,7 +97,7 @@ function App() {
         const interval = setInterval(() => {
             for (let i = 0; i < targets.length; i++)
             {
-                if(!getPostFirstLaunch[i]) { // spreading out request to avoid ERR_INSUFFICIENT_RESOURCES
+                if(!getPostFirstLaunch[i]) { // spreading out requests to avoid ERR_INSUFFICIENT_RESOURCES
                     const timeOut = setTimeout(() => {check(i, timeOut)}, Math.round(Math.random() * i * i * RETEST_OFFLINE_SITE))
                 }
 
@@ -122,22 +118,22 @@ function App() {
     <div className="wrapper d-flex justify-content-center mt-3 mb-3">
         <div className="d-flex flex-column align-items-center w-100">
             <div className="d-flex justify-content-center gap-3 mt-1 w-100 mb-3">
-                <div className="header cell text-start" style={{minWidth: "200px"}}>Website</div>
+                <div className="header cell text-start" style={{width: "200px"}}>Website</div>
                 <div className="header cell">Status</div>
                 <div className="header cell">Requests</div>
                 <div className="header cell">Errors</div>
             </div>
             {
                 (getRequests.length > 0 && getTargets.length > 0) ? getRequests.map((req, index) => {
-                    return <div className="d-flex justify-content-center gap-3 mt-1 w-100" key={index}>
-                        <div className="cell text-start" style={{minWidth: "200px"}}><a href={"http://"+getTargets[index]} target="_blank">{getTargets[index]}</a></div>
+                    return <div className="d-flex align-items-center justify-content-center gap-3 mt-1 w-100" key={index}>
+                        <div className="cell text-start" style={{width: "200px"}}><a href={"http://"+getTargets[index]} rel="noreferrer" target="_blank" title={getTargets[index]}>{getTargets[index].length > 25 ? `${getTargets[index].substring(0, 25)}...` : getTargets[index]}</a></div>
                         <div className="cell">{req.online === -1 ? <span style={{color: "grey"}}>Checking...</span> : req.online ? <span className="text-success">Online</span> : <span className="text-danger">Unreachable</span>}</div>
                         <div className="cell">{req.requests}</div>
                         <div className="cell">{req.errors}</div>
                     </div>
                 })
                 :
-                <div>WAITING FOR TARGETS...</div>
+                <div>LOADING...</div>
             }
             </div>
     </div>
